0886fd8cef9e2fdf1da41a92a2f13642a67dd0e8
Josckar <jp5936@nyu.edu>
2024-07-02 19:48:44 -0400

    remove console.log of fullUrl

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,7 +3,7 @@ const address = import.meta.env.VITE_BACKEND_APP_ADDRESS;
 const port = (':', import.meta.env.VITE_BACKEND_APP_PORT) || '';
 
 const fullUrl = `${address}${port}`;
-console.log(fullUrl);
+
 const api = axios.create({
     baseURL: fullUrl,
     withCredentials: true, 
